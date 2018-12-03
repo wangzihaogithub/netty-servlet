6d6948cf21acecbc3f1ff73796f91c5a899e1532
王子豪 <zhwang@kunfangtech.cn>
2018-12-03 16:09:05 +0800

    1.服务器启动后，控制台打印所有协议名称

--- src/main/java/com/github/netty/springboot/server/NettyEmbeddedServletContainer.java
+++ src/main/java/com/github/netty/springboot/server/NettyEmbeddedServletContainer.java
@@ -89,8 +89,8 @@ public class NettyEmbeddedServletContainer extends AbstractNettyServer implement
             PlatformDependent.throwException(cause);
         }
 
-        String protocols = String.join(",",protocolsRegisterList.stream().map(ProtocolsRegister::getProtocolName).collect(Collectors.toList()));
-        logger.info("{0} start [port = {1}, pid = {2}, protocol = {3}, os = {4} ]...",
+        List<String> protocols = protocolsRegisterList.stream().map(ProtocolsRegister::getProtocolName).collect(Collectors.toList());
+        logger.info("{0} start (port = {1}, pid = {2}, protocol = {3}, os = {4}) ...",
                 getName(),
                 getPort()+"",
                 HostUtil.getPid()+"",

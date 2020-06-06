e98a83ea3104e293382175c2600dff09e821d9f0
wangzihao <wangzihao@lechun.cc>
2020-06-06 18:40:31 +0800

    获取文件上传数据的例子

--- src/test/java/com/github/netty/http/MyHttpServlet.java
+++ src/test/java/com/github/netty/http/MyHttpServlet.java
@@ -4,11 +4,19 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 import java.io.IOException;
+import java.util.Collection;
 
 public class MyHttpServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Collection<Part> parts = request.getParts();
+        for (Part part : parts) {
+            String submittedFileName = part.getSubmittedFileName();
+            System.out.println("submittedFileName = " + submittedFileName);
+        }
+
         String name = request.getParameter("name");
         response.getWriter().write("hi! " + name);
     }

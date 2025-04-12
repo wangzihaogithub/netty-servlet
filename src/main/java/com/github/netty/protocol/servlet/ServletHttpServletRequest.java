1a25793b10740e00681503ecff963f9a9d6a4cdb
wangzihao <842156727@qq.com>
2025-04-12 19:15:49 +0800

    同步最新代码, 从 https://github.com/wangzihaogithub/spring-boot-protocol.git 当前项目是servlet项目, spring-boot-protocol项目是tcp协议项目,包含servlet

--- src/main/java/com/github/netty/protocol/servlet/ServletHttpServletRequest.java
+++ src/main/java/com/github/netty/protocol/servlet/ServletHttpServletRequest.java
@@ -514,7 +514,7 @@ public class ServletHttpServletRequest implements HttpServletRequest, Recyclable
         } else {
             queryString = null;
         }
-        this.requestURI = ServletContext.normPath(requestURI);
+        this.requestURI = requestURI;
         this.queryString = queryString;
         this.decodePathsFlag = true;
     }
